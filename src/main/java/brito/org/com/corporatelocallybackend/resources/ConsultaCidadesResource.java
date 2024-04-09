2af1e9d558200e7f802e73583673073ea976ea77
patrick <patrickcbrito88@gmail.com>
2024-04-09 09:58:51 -0300

    feat: ajuste nos imports dos resources

--- src/main/java/brito/org/com/corporatelocallybackend/resources/ConsultaCidadesResource.java
+++ src/main/java/brito/org/com/corporatelocallybackend/resources/ConsultaCidadesResource.java
@@ -1,16 +1,11 @@
 package brito.org.com.corporatelocallybackend.resources;
 
 import brito.org.com.corporatelocallybackend.dtos.TemperaturaAtualCidadeDTO;
-import brito.org.com.corporatelocallybackend.dtos.TemperaturaCidadeApiDTO;
-import brito.org.com.corporatelocallybackend.http.ConsultaTemperaturaCidadesHttp;
 import brito.org.com.corporatelocallybackend.services.ConsultaTemperaturaCidadeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/${application.path}/cidades")
