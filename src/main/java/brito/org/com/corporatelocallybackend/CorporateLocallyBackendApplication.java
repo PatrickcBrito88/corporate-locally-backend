041042408ce7c26de19484b45030e692134e7b2e
patrick <patrickcbrito88@gmail.com>
2024-04-09 10:02:54 -0300

    feat: remocao de properties no main

--- src/main/java/brito/org/com/corporatelocallybackend/CorporateLocallyBackendApplication.java
+++ src/main/java/brito/org/com/corporatelocallybackend/CorporateLocallyBackendApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.util.Properties;
-
 @SpringBootApplication
 @EnableFeignClients
 public class CorporateLocallyBackendApplication {
 
 	public static void main(String[] args) {
-		Properties properties = new Properties();
 		SpringApplication.run(CorporateLocallyBackendApplication.class, args);
 	}
 
